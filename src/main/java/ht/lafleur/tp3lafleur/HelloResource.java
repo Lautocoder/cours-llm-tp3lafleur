6aabcd91963f90edee097a9bca3d4a6eadcc651a
Stanley LAFLEUR <slafleur@solutions.ht>
2026-05-12 13:49:50 -0400

    Test d'une route avec parametre

--- src/main/java/ht/lafleur/tp3lafleur/HelloResource.java
+++ src/main/java/ht/lafleur/tp3lafleur/HelloResource.java
@@ -2,13 +2,18 @@ package ht.lafleur.tp3lafleur;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello-world")
 public class HelloResource {
+
     @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Produces("application/json")
+    @Path("personnes/{nom}")
+    public String hello(@PathParam("nom") String nom) {
+        // Créer un message de bienvenue
+        return "Bonjour, " + nom + '!';
     }
 }
